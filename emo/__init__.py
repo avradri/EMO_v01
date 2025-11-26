aedf9839d36faf0e5d0e7095b3eb5311dd3dda9c
avradri <avram3scu@outlook.com>
2025-11-26 11:33:33 +0100

    Add EMO package initializer (__init__.py)
    Created the initial __init__.py file for the EMO v0.1 package.
    This defines the public modules, sets the package version, and
    provides a clean entry point for the metric components (OI, Synergy,
    GWI, SMF, τ_I). This is the first step toward a functional EMO core.

---
+++ emo/__init__.py
@@ -0,0 +1,23 @@
+"""
+EMO v0.1 - Emergent Mind Observatory core package.
+
+This package provides metric computations for:
+- Organismality Index (OI)
+- Synergy / O-information-like indicator
+- Global Workspace Ignition (GWI)
+- Self-Model Fidelity (SMF)
+- Information-time (τ_I)
+"""
+
+__all__ = [
+    "config",
+    "utils",
+    "data_sources",
+    "organismality",
+    "synergy",
+    "gwi",
+    "smf",
+    "info_time",
+]
+
+__version__ = "0.1.0"
